3a62b8bcf95f3232346cb2ed9765539377507f81
suraj270702 <surajmaurya2707@eng.rizvi.edu.in>
2023-07-05 18:40:21 +0530

    complete api setup for products section

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,5 +1,6 @@
 const Product = require('../models/productsmodel')
-
+const productsSchema = require('../models/productsmodel')
+//create a product 
 exports.createProduct = async(req,res)=>{
     const product = await Product.create(req.body)
     res.status(201).json({
@@ -7,11 +8,14 @@ exports.createProduct = async(req,res)=>{
         product
     })
 }
+
+//function for fetching products
 exports.getAllProducts = async (req,res) =>{
     const products = await Product.find()
     res.status(200).json({success : true,products})
 }
 
+//function for updating products
 exports.updateProduct = async (req,res) =>{
     let product = await Product.findById(req.params.id);
     if(!product){
@@ -25,4 +29,27 @@ exports.updateProduct = async (req,res) =>{
         success : true,
         product
     })
+
+}
+//function for deleting a Product
+exports.deleteProduct = async(req,res) =>{
+    const {id} = req.params;
+    productsSchema.findByIdAndDelete(id).then((product)=>{
+        res.status(200).json({message : "Income Data Deleted Successfully !!"})
+    }).catch((error)=>{
+        res.status(500).json({message : "Server Error !!"})
+    })
+}
+
+//function for fetching a single product
+
+exports.getProductDetails = async(req,res) => {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success : true,
+            message : "Product Not Found"
+        })
+    }
+    res.status(200).json({success:true,message : "product found successfully"})
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const { getAllProducts,createProduct,updateProduct } = require('../controllers/productsControllers')
+const { getAllProducts,createProduct,updateProduct, deleteProduct, getProductDetails } = require('../controllers/productsControllers')
 const router = express.Router()
 router.route("/create-product").post(createProduct)
 router.route("/products").get(getAllProducts)
 router.route("/product/:id").put(updateProduct)
+router.route("/delete-product/:id").delete(deleteProduct)
+router.route("/productdetail/:id").get(getProductDetails)
 module.exports = router
