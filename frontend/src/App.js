4eba8cb4729bfb93cdecf9e17c21a9b8227462ea
suraj270702 <surajmaurya2707@eng.rizvi.edu.in>
2023-08-01 00:20:24 +0530

    Intial Frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import "./App.css";
+import Collections from "./components/Collections";
+import Navbar from "./components/Navbar";
+import Navigation from "./components/Navigation";
+import Promotion from "./components/Promotion";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Promotion />
+      <Collections />
+      
+    </>
+  );
+}
+
+export default App;
